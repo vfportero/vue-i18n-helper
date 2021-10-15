378dc3221d546f9ef96798b50f33ab61284de34f
Víctor Fernández Portero <victor.fernandez@lucera.es>
2021-10-15 17:48:55 +0200

    allow replace literals with inlined variables

--- src/localize.command.ts
+++ src/localize.command.ts
@@ -3,69 +3,87 @@ import { LocaleMessages } from './models';
 
 export class LocalizeCommand {
 
-  prefix = '{{ $t(\'';
-  suffix = '\') }}';
 
   public localizeText(textEditor: vscode.TextEditor) {
 
 		const document = textEditor.document;
-		const newSelections: vscode.Selection[] = [];
 
-		
-		textEditor.edit(editBuilder => {
-			textEditor.selections.forEach(selection => {
-				editBuilder.insert(selection.start, this.prefix);
-				editBuilder.insert(selection.end, this.suffix);
-				newSelections.push(new vscode.Selection(selection.start.translate(0, 0), 
-				selection.end.translate(0, this.prefix.length + this.suffix.length)));
-				
-				const textToLocalize = document.getText(selection);
+    textEditor.edit(editBuilder => {
+      textEditor.selections.forEach(selection => {
+        const textToLocalize = this.replaceSelector(document, editBuilder, selection);
 
-				let i18nTagStart;
-				let i18nTagEnd;
-
-				for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
-					const line = document.lineAt(lineNumber);
-					if (line.text.match(/<i18n>/)) {
-						i18nTagStart = line.range.start;
-					}
-					if (line.text.match(/<\/i18n>/)) {
-						i18nTagEnd = line.range.end;
-					}				
-        }
-
-        let i18nTranslations: LocaleMessages = {
-          ['es']: {},
-          ['eu']: {}
-        };
-
-        let i18nTagRange: vscode.Range;
-
-				if (i18nTagStart && i18nTagEnd) {
-					i18nTagRange = new vscode.Range(i18nTagStart, i18nTagEnd);
-					let i18nTag = document.getText(i18nTagRange);
-					
-					let i18nValues = i18nTag.replace('<i18n>', '');
-          i18nValues = i18nValues.replace('</i18n>', '');
-          i18nTranslations = {
-            ...i18nTranslations,
-            ...JSON.parse(i18nValues)
-          };
-        } else {
-          i18nTagRange = new vscode.Range(new vscode.Position(document.lineCount,0), new vscode.Position(document.lineCount,0));
-        }
+        let currentFileTranslations = this.getCurrentFileTranslations(document);
         
-        i18nTranslations['es'][textToLocalize] = textToLocalize;
-        i18nTranslations['eu'][textToLocalize] = textToLocalize;
-        let newi18nTag = `<i18n>\r\n${JSON.stringify(i18nTranslations, null, 2)}\r\n</i18n>`;
+        currentFileTranslations.i18nTranslations['es'][textToLocalize] = textToLocalize;
+        currentFileTranslations.i18nTranslations['eu'][textToLocalize] = textToLocalize;
+        let newi18nTag = `<i18n>\r\n${JSON.stringify(currentFileTranslations.i18nTranslations, null, 2)}\r\n</i18n>`;
 
-        editBuilder.replace(i18nTagRange, newi18nTag);
-        
+        editBuilder.replace(currentFileTranslations.i18nTagRange, newi18nTag);
 				
-				vscode.window.showInformationMessage(`Added localized key '${textToLocalize}'`);
-			});
-		}).then(() => {
-			textEditor.selections = newSelections;
-		});
+        vscode.window.showInformationMessage(`Added localized key '${textToLocalize}'`);
+      });
+    });
+  }
+
+  private replaceSelector(document: vscode.TextDocument, editBuilder: vscode.TextEditorEdit, selection: vscode.Selection): string {
+    let textToReplace = document.getText(selection);
+    const inlinedVariableRegExp = new RegExp(/.({{[^}]*}})/);
+    let inlineVariableNames = [];
+
+    let inlinedVariableMatch = inlinedVariableRegExp.exec(textToReplace);
+
+    while (inlinedVariableMatch !== null) {   
+      let inlineVariable = inlinedVariableMatch[1];
+      const inlineVariableName = inlineVariable.replace('{{', '').replace('}}', '');
+      inlineVariableNames.push(inlineVariableName);
+      textToReplace = textToReplace.replace(inlineVariable, `{${inlineVariableName}}`);
+      inlinedVariableMatch = inlinedVariableRegExp.exec(textToReplace);
+    }
+
+    if (inlineVariableNames.length) {
+      editBuilder.replace(selection, `{{ $t(\'${textToReplace}\', { ${inlineVariableNames.join(',')} })}}`);
+    } else {
+      editBuilder.replace(selection, `{{ $t(\'${textToReplace}\')}}`);
+    }
+    
+    return textToReplace;
+  }
+
+  private getCurrentFileTranslations(document: vscode.TextDocument): { i18nTranslations: LocaleMessages, i18nTagRange: vscode.Range }   {
+    let i18nTagStart;
+    let i18nTagEnd;
+
+    for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+      const line = document.lineAt(lineNumber);
+      if (line.text.match(/<i18n>/)) {
+        i18nTagStart = line.range.start;
+      }
+      if (line.text.match(/<\/i18n>/)) {
+        i18nTagEnd = line.range.end;
+      }				
+    }
+
+    let i18nTranslations: LocaleMessages = {
+      ['es']: {},
+      ['eu']: {}
+    };
+
+    let i18nTagRange: vscode.Range;
+
+    if (i18nTagStart && i18nTagEnd) {
+      i18nTagRange = new vscode.Range(i18nTagStart, i18nTagEnd);
+      let i18nTag = document.getText(i18nTagRange);
+      
+      let i18nValues = i18nTag.replace('<i18n>', '');
+      i18nValues = i18nValues.replace('</i18n>', '');
+      i18nTranslations = {
+        ...i18nTranslations,
+        ...JSON.parse(i18nValues)
+      };
+    } else {
+      i18nTagRange = new vscode.Range(new vscode.Position(document.lineCount,0), new vscode.Position(document.lineCount,0));
+    }
+
+    return { i18nTranslations, i18nTagRange };
   }
 }
